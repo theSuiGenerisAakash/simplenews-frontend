e0ca026e63d846230302a23dec6e0e218c2411df
Aakash Verma <Aakash_Verma@mckinsey.com>
2020-08-06 14:14:57 +0530

    (fix) add missing files from previous commit - toastify

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import * as React from "react"
 import { render } from "react-dom"
 import App from "./components/App"
-import AsyncLoader from "./pages/AsyncLoader"
+import AsyncLoaderPage from "./pages/AsyncLoaderPage"
+import { ToastContainer } from "react-toastify"
 const rootEl = document.getElementById("root")
 
 render(
     <>
         <App />
-        <AsyncLoader />
+        <ToastContainer />
+        <AsyncLoaderPage />
     </>,
     rootEl
 )
